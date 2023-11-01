a0ae2cd5497f4337098592a560797dc05fd5785e
OlaGreat <oladipupoolamilekan2@gmail.com>
2023-11-01 07:50:37 +0100

    i updated the productRequest class

--- src/main/java/com/fiddovea/fiddovea/dto/request/ProductRequest.java
+++ src/main/java/com/fiddovea/fiddovea/dto/request/ProductRequest.java
@@ -1,18 +1,15 @@
 package com.fiddovea.fiddovea.dto.request;
 
-import com.fiddovea.fiddovea.data.models.Review;
-import com.fiddovea.fiddovea.data.models.Vendor;
+
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
-import java.util.List;
+
 @Setter
 @Getter
 public class ProductRequest {
-
     private String productName;
     private String productDescription;
     private BigDecimal productPrice;
